bb0dcff966578d70afcce09be36649d32d47c531
Gyeongseo Bak <rudtj2966@gmail.com>
2023-01-02 20:08:51 +0900

    init project

---
+++ src/main/java/com/nhnacademy/bookpubdelivery/BookpubDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookpubdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookpubDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookpubDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/bookpubdelivery/BookpubDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookpubdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookpubDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
