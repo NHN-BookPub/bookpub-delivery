41b2d22e935d8574bf8f1f7ae20a6c900bfe4fd0
Gyeongseo Bak <rudtj2966@gmail.com>
2023-01-03 21:47:34 +0900

    [Delete] 주석 삭제

--- src/main/java/com/nhnacademy/bookpubdelivery/BookpubDeliveryApplication.java
+++ src/main/java/com/nhnacademy/bookpubdelivery/BookpubDeliveryApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookpubDeliveryApplication {
 
-	// 주석
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookpubDeliveryApplication.class, args);
 	}
