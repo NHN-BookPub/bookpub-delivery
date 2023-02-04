4498767d4d4daf3410d82d11906b3e10845543d6
HoFe-U <hochul-y@naver.com>
2023-02-04 20:14:54 +0900

    [Dto] : 배송지역반환 dto

---
+++ src/main/java/com/nhnacademy/bookpubdelivery/deliverylocation/dto/response/DeliveryLocationResponseDto.java
@@ -0,0 +1,11 @@
+package com.nhnacademy.bookpubdelivery.deliverylocation.dto.response;
+
+/**
+ * 배송지역반환 dto
+ * View 를 하게되면 수정예정
+ *
+ * @author : 유호철
+ * @since : 1.0
+ **/
+public class DeliveryLocationResponseDto {
+}
