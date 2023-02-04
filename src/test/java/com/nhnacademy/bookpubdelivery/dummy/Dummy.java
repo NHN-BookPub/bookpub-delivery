e276ff5af0459f3b07002dc3b7c317bc6b24cc42
HoFe-U <hochul-y@naver.com>
2023-02-04 20:14:04 +0900

    [Test] : 배송관련 더미

---
+++ src/test/java/com/nhnacademy/bookpubdelivery/dummy/Dummy.java
@@ -0,0 +1,29 @@
+package com.nhnacademy.bookpubdelivery.dummy;
+
+import com.nhnacademy.bookpubdelivery.delivery.entity.Delivery;
+import com.nhnacademy.bookpubdelivery.deliverystate.entity.DeliveryState;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * 배송 더미
+ * 
+ * @author : 유호철
+ * @since : 1.0
+ **/
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Dummy {
+    public static Delivery deliveryDummy(){
+        return Delivery.builder()
+                .orderNo(1L)
+                .recipient("수령인")
+                .requestDate(null)
+                .company("회사")
+                .finishedAt(LocalDateTime.now())
+                .invoiceNo("uuid")
+                .phone("phone")
+                .state(new DeliveryState(null,"state"))
+                .build();
+    }
+}
